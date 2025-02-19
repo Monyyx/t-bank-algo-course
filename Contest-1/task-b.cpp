e2536e84b10a124c609b4ca3e5421af24e28b482
Monyyx <pppprtoyy@gmail.com>
2025-02-19 11:13:23 +0300

    AAA

--- Contest-1/task-b.cpp
+++ Contest-1/task-b.cpp
@@ -5,23 +5,27 @@
 #define vector std::vector
 #define string std::string
 
-string binSearch(vector<ll> input, ll num){
+int findingNum(vector<ll> in, ll num) {
   ll left = 0;
-  ll right = input.size() - 1;
+  ll right = in.size() - 1;
+  ll  closest = -1000000000000;
   
   while (left <= right) {
     ll mid = left + (right - left) / 2;
-    if (input[mid] == num) {
-      return "YES";
-    } else if (input[mid] > num) {
+    if (in[mid] == num) {
+        return num;
+    } else if (in[mid] > num) {
       right = mid - 1;
+      if (std::abs(in[mid] - num) < std::abs(closest - num))  closest  = in[mid];
     } else {
       left = mid + 1;
+      if (std::abs(in[mid] - num) < std::abs(closest - num))  closest  = in[mid];
     }
   }
 
-  return "NO";
+  return closest;
 }
+
 
 int main(){
   std::ios::sync_with_stdio(false);
@@ -29,11 +33,11 @@ int main(){
 
   ll n, k;
   std::cin >> n >> k;
-  vector<ll> input(n);
+  vector<ll> in(n); //  up sort
   vector<ll> find(k);
 
   for (ll i = 0; i < n; ++i) {
-    std::cin >> input[i];
+    std::cin >> in[i];
   }
 
   for (ll i = 0; i < k; ++i) {
@@ -41,6 +45,7 @@ int main(){
   }
 
   for (ll i = 0; i < k; ++i) {
-    std::cout << binSearch(input, find[i]) << '\n';
+    std::cout << findingNum(in, find[i]) << '\n';
   }
+  
 }
